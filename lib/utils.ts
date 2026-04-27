34e636c65883a5eb8b5085c42e51de041f3986e5
assbee00 <niuniu9785@hotmail.com>
2026-04-27 20:59:39 +0900

    中文适配

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,23 +1,4 @@
-import fs from "fs";
-import path from "path";
 import { JSX } from "react";
-
-
-export function getClassData(className: string) {
-    const filePath = path.join(process.cwd(), "data", `${className}.json`);
-    const json = fs.readFileSync(filePath, "utf-8");
-    return JSON.parse(json);
-}
-
-export function getOrderData(order: string) {
-    const filePath = path.join(process.cwd(), "data/orders", `${order}.json`);
-    const json = fs.readFileSync(filePath, "utf-8");
-    return JSON.parse(json);
-}
-
-export function getAllClasses() {
-    return ["aves", "mammalia"];
-}
 
 export function getHeadingTag(depth: number): keyof JSX.IntrinsicElements {
     const clamped = Math.min(Math.max(depth, 0), 5);
